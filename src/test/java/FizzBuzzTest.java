3e0a364ab1a41a4b4c4e8491f90c9fb2d6f1e947
jmccann-dev-10 <jmccann@dev-10.com>
2021-07-22 11:31:36 -0500

    made unit tests more descriptive

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -6,40 +6,49 @@ import static learn.fizzbuzz.FizzBuzz.*;
 public class FizzBuzzTest {
 
     @Test
-    public void multiplesOfThreeShouldReturnFizz() {
+    public void shouldBeFizzWhen3() {
         int num = 3;
         String result = processFizzBuzz(num);
         assertEquals(result, FIZZ);
+    }
 
-        num *= 4;
-        result = processFizzBuzz(num);
+    @Test
+    public void shouldBeFizzWhenMultipleOf3() {
+        int num = 3 * 4;
+        String result = processFizzBuzz(num);
         assertEquals(result, FIZZ);
     }
 
     @Test
-    public void multiplesOfFiveShouldReturnBuzz() {
+    public void shouldBeBuzzWhen5() {
         int num = 5;
         String result = processFizzBuzz(num);
         assertEquals(result, BUZZ);
+    }
 
-        num *= 4;
-        result = processFizzBuzz(num);
+    @Test
+    public void shouldBeBuzzWhenMultipleOf5() {
+        int num = 5 * 4;
+        String result = processFizzBuzz(num);
         assertEquals(result, BUZZ);
     }
 
     @Test
-    public void multipleOfThreeAndFiveShouldReturnFizzBuzz() {
-        int num = 15;
+    public void shouldReturnFizzBuzzWhenProductOf3And5() {
+        int num = 3 * 5;
         String result = processFizzBuzz(num);
-        assertEquals(result, FIZZ + BUZZ);
-
-        num *= 4;
-        result = processFizzBuzz(num);
         assertEquals(result, FIZZ + BUZZ);
     }
 
     @Test
-    public void anyOtherNumberShouldBeReturned() {
+    public void shouldReturnFizzBuzzWhenMultipleOf3And5() {
+        int num = 5 * 7 * 3 * 4;
+        String result = processFizzBuzz(num);
+        assertEquals(result, FIZZ + BUZZ);
+    }
+
+    @Test
+    public void shouldReturnNumberWhenNotMultipleOf3Or5() {
         int num = 11;
         String result = processFizzBuzz(num);
         assertEquals(Integer.toString(num), result);
@@ -50,13 +59,12 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void zeroShouldNotReturnFizzOrBuzz() {
+    public void shouldNotReturnFizzOrBuzzWhen0() {
         int num = 0;
         String result = processFizzBuzz(num);
         assertNotEquals(result, FIZZ);
         assertNotEquals(result, BUZZ);
         assertNotEquals(result, FIZZ + BUZZ);
-        assertEquals(result, Integer.toString(num));
     }
 
 }
